d1e7ab1e2bdac3e42db8bbe36e74f4e82cefa9b6
Tom <tomgriffiths2007@gmail.com>
2026-01-17 03:20:04 +0000

    v7
    Updated for server v12

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ This is a package for PHP-CLI.
 # Functions
 - **runcommand(string $command, string $ip="127.0.0.1", int $port=8080):bool**: Tells communicator server to run a cli command, returns true if it was able to contact communicator server or false on failure.
 - **runfunction(string $function, string $ip="127.0.0.1", int $port=8080):mixed**: Tells communicator server to run a function string, returns the return of the function on success or false on failure.
+- **serverCalledMe():bool**: Returns true if communicator_server class is anywhere in the backtrace, false otherwise. This is usefull for making sure communicator_server doesnt end up calling itself.
--- main.php
+++ main.php
@@ -1,67 +1,104 @@
 <?php
 class communicator_client{
-    public static function runfunction(string $function, string $ip="127.0.0.1", int $port=8080):mixed{
-        $result = self::run($ip, $port, array("type"=>"function_string","payload"=>$function));
+    public static function runfunction(string $function, string $ip="127.0.0.1", int $port=8080, bool $returnErrorString=false):mixed{
+
+        $result = self::run($ip, $port, ["type"=>"function_string", "payload"=>$function]);
+
         if($result["success"]){
             return $result["result"];
         }
+
+        if(isset($result['error']) && is_string($result['error'])){
+            if($returnErrorString){
+                return $result['error'];
+            }
+            else{
+                mklog(0, 'Runfunction Error: ' . $result['error']);
+            }
+        }
+
         return false;
     }
-    public static function runcommand(string $command, string $ip="127.0.0.1", int $port=8080):bool{
-        $result = self::run($ip, $port, array("type"=>"command","payload"=>$command));
+    public static function runcommand(string $command, string $ip="127.0.0.1", int $port=8080, bool $returnOutput=false):bool|string{
+
+        $result = self::run($ip, $port, ["type"=>"command" . ($returnOutput ? "_output" : ""), "payload"=>$command]);
+
+        if($returnOutput){
+            if(is_string($result['result'])){
+                return $result['result'];
+            }
+            else{
+                return "";
+            }
+        }
+
         return $result["success"];
     }
-    public static function run(string $ip, int $port, array $data, float|false $timeout=false):array{
-        $socket = communicator::connect($ip, $port, $timeout, $socketError, $socketErrorString);
-        if($socket !== false){
-            return self::execute($socket, $data);
+    public static function serverCalledMe():bool{
+        foreach(debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS) as $caller){
+            if(isset($caller['class']) && $caller['class'] === "communicator_server"){
+                return true;
+            }
         }
-        return ["success"=>false, "error"=>"Unable to connect to " . $ip . ":" . $port];
+
+        return false;
+    }
+    public static function run(string $ip, int $port, array $data, float|false $timeout=false):array{
+
+        $data['version'] = 2;
+
+        //Check if call originates from communicator_server to avoid communicator talking to itself while the server is in a busy state
+        if(self::serverCalledMe()){
+            mklog(0, "Running communicator_server action internally");
+            if(method_exists('communicator_server', 'run')){
+                return communicator_server::run($data);
+            }
+            else{
+                return ["success"=>false, "error"=>"Cannot connect to communicator_server from within communicator_server with version 11 or lower"];
+            }
+        }
+        else{
+            $socket = communicator::connect($ip, $port, $timeout, $socketError, $socketErrorString);
+
+            if($socket === false){
+                return [
+                    "success" => false,
+                    "error" => "Unable to connect to " . $ip . ":" . $port
+                ];
+            }
+            
+            $result = self::execute($socket, $data);
+
+            if(!communicator::close($socket)){
+                mklog(2, "Failed to close socket");
+            }
+
+            return $result;
+        }
     }
     private static function execute($socket, array $data):array{
-        $return = array("success"=>false);
 
         if(!isset($data['type'])){
-            $return["error"] = "Type not set";
-            goto end;
-        }
-        if(!in_array($data['type'],array("function_string","command","stop"))){
-            $return["error"] = "Type not recognised";
-            goto end;
+            return ["success"=>false, "error"=>"Type not set"];
         }
 
         if(!isset($data['payload'])){
-            $return["error"] = "Payload not set";
-            goto end;
+            return ["success"=>false, "error"=>"Payload not set"];
         }
 
-        $data['name'] = communicator::getName();
-        $data['password'] = communicator::getPasswordEncoded();
-
-        $data = base64_encode(json_encode($data));
-
-        if(!communicator::send($socket,$data)){
-            $return["error"] = "Error sending data";
-            goto end;
+        if(!communicator::sendData($socket, $data, true)){
+            return ["success"=>false, "error"=>"Error sending data"];
         }
 
-        $result = communicator::receive($socket);
-        if($result === false){
-            $return["error"] = "Error receiving data";
-            goto end;
+        $result = communicator::receiveData($socket, true);
+        if(!is_array($result)){
+            return ["success"=>false, "error"=>"Error receiving data"];
         }
 
-        $result = json_decode(base64_decode($result),true);
-        if($result === null){
-            $return["error"] = "Empty response";
-            goto end;
+        if(!isset($result['success']) || !isset($result['result'])){
+            return ["success"=>false, "error"=>"Received incomplete data"];
         }
 
-        $return["success"] = true;
-        $return["result"] = $result;
-
-        end:
-        communicator::close($socket);
-        return $return;
+        return $result;
     }
 }
